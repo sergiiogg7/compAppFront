207b0445b2936ecddd12aafb9d985276b07f5cac
Sergio Garcia <sergiogarciagasco14@gmail.com>
2024-01-22 22:53:06 +0100

    create dropdownmenu structure component

---
+++ app/ui/DropDownMenu.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function DropDownMenu() {
+  return <></>;
+}

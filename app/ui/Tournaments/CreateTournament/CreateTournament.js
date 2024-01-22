fc829043cf394bbac19cd71f3e7580e11dbbdd5e
Sergio Garcia <sergiogarciagasco14@gmail.com>
2024-01-22 22:52:33 +0100

    structure of create tournament

---
+++ app/ui/Tournaments/CreateTournament/CreateTournament.js
@@ -0,0 +1,19 @@
+import { Title, Card, Flex, Text, Subtitle } from "@tremor/react";
+import React, { useState } from "react";
+
+const CreateTournament = ({}) => {
+  return (
+    <>
+      <div>
+        {/* NAME & END DATE */}
+        <div></div>
+        {/* DESCRIPTION */}
+        <div></div>
+        {/* TROPIES */}
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default CreateTournament;
